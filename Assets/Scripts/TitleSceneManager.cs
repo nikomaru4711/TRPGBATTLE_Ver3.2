f20263d24ad86fad0985dd124d4417b0d8f57f91
nikomaru4711 <nikowanngt@gmail.com>
2025-07-29 15:55:42 +0900

    がんばったぽに！
    今日は一旦ここまでぽに～

--- Assets/Scripts/TitleSceneManager.cs
+++ Assets/Scripts/TitleSceneManager.cs
@@ -2,10 +2,17 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using UniRx;
+using System;
 public class TitleSceneManager : MonoBehaviour
 {
+    public enum State
+    {
+        phase1,
+        phase2
+    }
     //アクセス修飾子の後にstaticでグローバル化
     public PlayerCharacter _playerJson;
     public NewSkill[] _skills = new NewSkill[50];
@@ -19,7 +26,17 @@ public class TitleSceneManager : MonoBehaviour
     [SerializeField] private GameObject _inputJson;
     [SerializeField] private GameObject _buttonStart;
     [SerializeField] private GameObject _validateErrorPanel;
+    [SerializeField] private GameObject _methodPref;
+    [SerializeField] private GameObject _registAtk;
+    [SerializeField] private GameObject _atkScrollView;
+    [SerializeField] private GameObject _scrollContent;
+    private GameObject _dump;
+    private List<GameObject> _atkMethodList = new List<GameObject>();
+    private List<string> _optionList = new List<string>();
+    private int _atkCount = 0;
     private string[] commands = new string[64];
+    private TMP_Dropdown _dropdown;
+    private State _state;
 
     //念のため、最初に見せないものを非表示にするよう記述
     private void Start()
@@ -30,96 +47,142 @@ public class TitleSceneManager : MonoBehaviour
     //プレイヤーを作成してゲーム開始。
     public void GameStart()
     {
-        //////////////////
-        //JSONの読み込み//
-        //////////////////
-        try
+        switch (_state)
         {
-            string _jsonText = _inputJson.GetComponent<TMP_InputField>().text.Replace("\"params\":", "\"param\":");
-            _playerJson = _readJson.ReadJsonFile(_jsonText);
-            if (_playerJson == null){ return; }
-            Debug.Log("JSONの読み込み完了");
-        }
-        catch (System.Exception e)
-        {
-            Debug.Log("JSONの読み込みに失敗");
-            Debug.Log(e.ToString());
-            ValidateErrorPanel(true);
-            return;
-        }
-
-
-        ///////////////////
-        //NewSkillsの作成//
-        ///////////////////
-        for (int i = 0;i < commands.Length;i++)
-        {
-            commands = _playerJson.data.commands.Split('\n');
-        }
-        NewSkill dump_newskill;
-        string marker = "CC<=";
-        string dump_text;
-        int startIndex;
-        int endIndex;
-        int index = 0;
-        for (int i = 0; i < commands.Length -12; i++)
-        {
-            if (i == 1 || (2 < i && 50 < i) ) 
-            {
-                startIndex = commands[i].IndexOf(marker);
-                endIndex = commands[i].IndexOf(" 【");
-
-                if (startIndex != -1 && endIndex != -1)
+            case State.phase1:
+                //////////////////
+                //JSONの読み込み//
+                //////////////////
+                try
                 {
-                    startIndex += marker.Length;
-                    dump_text = commands[i].Substring(startIndex, endIndex - startIndex);
+                    string _jsonText = _inputJson.GetComponent<TMP_InputField>().text.Replace("\"params\":", "\"param\":");
+                    _playerJson = _readJson.ReadJsonFile(_jsonText);
+                    if (_playerJson == null) { return; }
+                    Debug.Log("JSONの読み込み完了");
                 }
-                else
-                {//エラー
-                    Debug.LogError("NewSkillの作成に失敗しました！");
-                    Debug.LogErrorFormat("i = {0}\ncommand[{1}] = {2}", i, i, commands[i]);
+                catch (System.Exception e)
+                {
+                    Debug.Log("JSONの読み込みに失敗");
+                    Debug.Log(e.ToString());
+                    ValidateErrorPanel(true);
                     return;
                 }
-                Debug.LogFormat("i = {0}を作成します。", i);
-                Debug.LogFormat("dump_text = {0}", dump_text);
-                dump_newskill = new NewSkill(commands[i], int.Parse(dump_text));
-                _skills[index] = dump_newskill;
-                index++;
-            } else 
-            {
-                
-            }
-            
-        }
-        //////////////////////
-        //NewCharacterの作成//
-        //////////////////////
-        _player = new NewCharacter(0, _playerJson.data.name, _playerJson.data.status[0].value, int.Parse(_playerJson.data.param[3].value), _playerJson.data.iconUrl, _skills, GameManager.CharacterKind.Player);
-        //////////////////
-        //攻撃方法の記入//
-        //////////////////
-        //IEnumerator Atk = AddAtk();
+                ///////////////////
+                //NewSkillsの作成//
+                ///////////////////
+                for (int i = 0; i < commands.Length; i++)
+                {
+                    commands = _playerJson.data.commands.Split('\n');
+                }
+                NewSkill dump_newskill;
+                string marker = "CC<=";
+                string dump_text;
+                int startIndex;
+                int endIndex;
+                int index = 0;
+                for (int i = 0; i < commands.Length - 12; i++)
+                {
+                    if (i == 1 || (2 < i && 50 < i))
+                    {
+                        startIndex = commands[i].IndexOf(marker);
+                        endIndex = commands[i].IndexOf(" 【");
 
-        Observable.FromCoroutine(AddAtk)
-            .Subscribe(_ =>
-            {
-                Debug.Log("バトルシーンへ遷移");
-                //SceneManager.LoadScene("Battle");
-            }).AddTo(this);
+                        if (startIndex != -1 && endIndex != -1)
+                        {
+                            startIndex += marker.Length;
+                            dump_text = commands[i].Substring(startIndex, endIndex - startIndex);
+                        }
+                        else
+                        {//エラー
+                            Debug.LogError("NewSkillの作成に失敗しました！");
+                            Debug.LogErrorFormat("i = {0}\ncommand[{1}] = {2}", i, i, commands[i]);
+                            return;
+                        }
+                        Debug.LogFormat("i = {0}を作成します。", i);
+                        Debug.LogFormat("dump_text = {0}", dump_text);
+                        dump_newskill = new NewSkill(commands[i], int.Parse(dump_text));
+                        _skills[index] = dump_newskill;
+                        index++;
+                    }
+                }
+                //////////////////////
+                //NewCharacterの作成//
+                //////////////////////
+                _player = new NewCharacter(0, _playerJson.data.name, _playerJson.data.status[0].value, int.Parse(_playerJson.data.param[3].value), _playerJson.data.iconUrl, _skills, GameManager.CharacterKind.Player);
+                ///////////////////////
+                //１．オプション追加 //
+                ///////////////////////
+                PhaseChange(State.phase2);
+                _dropdown = _methodPref.gameObject.transform.GetChild(1).gameObject.transform.GetChild(0).gameObject.GetComponent<TMP_Dropdown>();
+                for (int i = 0; i < commands.Length; i++)
+                {
+                    _optionList.Add(commands[i]);
+                }
+                _dropdown.ClearOptions();
+                _dropdown.AddOptions(_optionList);
+                _state = State.phase2;
+                break;
+            case State.phase2:
+                break;
+        }
 
         
-    }
-    //攻撃方法に関して記載する。
-    private IEnumerator AddAtk()
-    {
-        _textTitle.SetActive(false);
-        _textDetail.SetActive(false);
-        _inputJson.SetActive(false);
-        Debug.Log("２秒待ち");
-        yield return new WaitForSeconds(2.0f);
-        Debug.Log("完了！");
+
+
     }
 
+    public void AddMethod()
+    {
+        _dump = Instantiate(_methodPref, new Vector3(0,0,0), Quaternion.identity);
+        _dump.transform.SetParent(_scrollContent.transform);
+        _dump.name = "AtkMethod_" + _atkCount;
+        _atkCount++;
+        _atkMethodList.Add(_dump);
+    }
+
+    public void RemoveMethod()
+    {
+        if(1 <= _atkCount)
+        {
+            _dump = _atkMethodList[_atkCount - 1];
+            _atkMethodList.RemoveAt(_atkCount - 1);
+            Destroy(_dump);
+            _atkCount--;
+        } else
+        {
+            Debug.LogError("これ以上削除するListがありません。");
+        }
+    }
+
+    //ScrollViewの画面へ
+    public void PhaseChange(State state)
+    {
+        switch (state)
+        {
+            case State.phase1:
+                _textDetail.SetActive(true);
+                _inputJson.SetActive(true);
+                _specialThanks.SetActive(true);
+                _registAtk.SetActive(false);
+                _buttonStart.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("Next");
+                _textTitle.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("TRPGの戦闘を抜き出してみたver3.2");
+                break;
+            case State.phase2:
+                _textDetail.SetActive(false);
+                _inputJson.SetActive(false);
+                _specialThanks.SetActive(false);
+                _registAtk.SetActive(true);
+                _buttonStart.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("GameStart");
+                _textTitle.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText("攻撃方法を追加してね");
+                break;
+        }
+    }
+
+    public void MovePhase1()
+    {
+        PhaseChange(State.phase1);
+        _state = State.phase1;
+    }
     //入力エラーの「分かった」を押したとき
     public void ValidateErrorPanel(bool index)
     {
